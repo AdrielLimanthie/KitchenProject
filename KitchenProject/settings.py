090f4b4b2ad2a2cb3a74ecfc45413c4594121ca7
Adriel Limanthie <a.limanthie@yahoo.com>
2016-08-21 20:40:46 +0700

    Edit database setting for Heroku

--- KitchenProject/settings.py
+++ KitchenProject/settings.py
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'KitchenProject.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd5059ghhccj86',
+        'USER': 'sojucpphtxjnnx',
+        'PASSWORD': 'tTXxEq8VviXBE6Zfmxc-GGFQKk',
+        'HOST': 'ec2-54-83-44-117.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
